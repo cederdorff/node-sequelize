6156dcd38c44f78a3ade923457835ba5a701a97c
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-11-12 11:49:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const User = sequelize.define("user", {
         allowNull: false // Email is required
     },
     image: {
-        type: DataTypes.STRING
+        type: DataTypes.TEXT // URL to image
     }
 });
 
