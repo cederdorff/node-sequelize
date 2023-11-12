3005606e40cc0ec5999ce1e73805844328aca5ec
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-11-12 11:49:26 +0100

    comments

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const User = sequelize.define("user", {
         allowNull: false // Email is required
     },
     image: {
-        type: DataTypes.STRING
+        type: DataTypes.TEXT //
     }
 });
 
@@ -114,111 +114,108 @@ app.get("/", (request, response) => {
 
 // READ all users
 app.get("/users", async (request, response) => {
-    const users = await User.findAll();
-
-    response.json(users);
+    const users = await User.findAll(); // SELECT * FROM users;
+    response.json(users); // send the users as JSON
 });
 
 // READ one user by id
 app.get("/users/:id", async (request, response) => {
-    const id = request.params.id;
-    const users = await User.findByPk(id);
-
-    response.json(users);
+    const id = request.params.id; // use id from url
+    const users = await User.findByPk(id); // SELECT * FROM users WHERE id = id;
+    response.json(users); // send the users as JSON
 });
 
 // CREATE one user
 app.post("/users", async (request, response) => {
-    const user = request.body;
-
-    const newUser = await User.create(user);
-    response.json(newUser);
+    const user = request.body; // use request body as user object
+    const newUser = await User.create(user); // INSERT INTO users (name, title, mail, image) VALUES (user.name, user.title, user.mail, user.image);
+    response.json(newUser); // send the new user as JSON
 });
 
 // UPDATE one user by id
 app.put("/users/:id", async (request, response) => {
-    const id = request.params.id;
-    const user = request.body;
+    const id = request.params.id; // use id from url
+    const user = request.body; // use request body as user object
 
-    const [result] = await User.update(user, { where: { id: id } });
+    const [result] = await User.update(user, { where: { id: id } }); // UPDATE users SET name = user.name, title = user.title, mail = user.mail, image = user.image WHERE id = id;
 
     if (result) {
-        response.json({ message: "User updated" });
+        response.json({ message: "User updated" }); // send a success message as JSON
     } else {
-        response.json({ message: "User not found" });
+        response.json({ message: "User not found" }); // send a not found message as JSON
     }
 });
 
 // DELETE user by id
 app.delete("/users/:id", async (request, response) => {
-    const id = request.params.id; // tager id fra url'en, så det kan anvendes til at finde den givne bruger med "det" id.
-    const result = await User.destroy({ where: { id: id } });
+    const id = request.params.id; // use id from url
+    const result = await User.destroy({ where: { id: id } }); // DELETE FROM users WHERE id = id;
 
     if (result) {
-        response.json({ message: "User deleted" });
+        response.json({ message: "User deleted" }); // send a success message as JSON
     } else {
-        response.json({ message: "User not found" });
+        response.json({ message: "User not found" }); // send a not found message as JSON
     }
 });
 
+// READ all posts
 app.get("/posts", async (request, response) => {
-    const posts = await Post.findAll({ include: User });
-
-    response.json(posts);
+    const posts = await Post.findAll({ include: User }); // SELECT * FROM posts;
+    response.json(posts); // send the posts as JSON
 });
 
+// READ one post by id
 app.get("/posts/:id", async (request, response) => {
-    const id = request.params.id;
-    const posts = await Post.findByPk(id, { include: User });
-
-    response.json(posts);
+    const id = request.params.id; // use id from url
+    const posts = await Post.findByPk(id, { include: User }); // SELECT * FROM posts WHERE id = id;
+    response.json(posts); // send the posts as JSON
 });
 
+// READ all posts for a user
 app.get("/users/:id/posts", async (request, response) => {
-    const id = request.params.id;
-    const user = await User.findByPk(id, { include: Post });
-
-    response.json(user);
+    const id = request.params.id; // use id from url
+    const user = await User.findByPk(id, { include: Post }); // SELECT * FROM users WHERE id = id;
+    response.json(user); // send the user as JSON
 });
 
+// CREATE one post for a given user
 app.post("/users/:id/posts", async (request, response) => {
-    const id = request.params.id;
-    const post = request.body;
+    const id = request.params.id; // use id from url
+    const post = request.body; // use request body as post object
+    const user = await User.findByPk(id); // SELECT * FROM users WHERE id = id;
 
-    const user = await User.findByPk(id);
-
-    const newPost = await Post.create(post);
-    newPost.setUser(user);
-
-    response.json(newPost);
+    const newPost = await Post.create(post); // INSERT INTO posts (caption, image) VALUES (post.caption, post.image);
+    newPost.setUser(user); // UPDATE posts SET userId = user.id WHERE id = newPost.id;
+    response.json(newPost); // send the new post as JSON
 });
 
+// CREATE one post
 app.post("/posts", async (request, response) => {
-    const post = request.body;
-
-    const newPost = await Post.create(post);
-    response.json(newPost);
+    const post = request.body; // use request body as post object
+    const newPost = await Post.create(post); // INSERT INTO posts (caption, image) VALUES (post.caption, post.image);
+    response.json(newPost); // send the new post as JSON
 });
 
+// UPDATE one post by id
 app.put("/posts/:id", async (request, response) => {
-    const id = request.params.id;
-    const post = request.body;
-
-    const [result] = await Post.update(post, { where: { id: id } });
+    const id = request.params.id; // use id from url
+    const post = request.body; // use request body as post object
+    const [result] = await Post.update(post, { where: { id: id } }); // UPDATE posts SET caption = post.caption, image = post.image WHERE id = id;
 
     if (result) {
-        response.json({ message: "Post updated" });
+        response.json({ message: "Post updated" }); // send a success message as JSON
     } else {
-        response.json({ message: "Post not found" });
+        response.json({ message: "Post not found" }); // send a not found message as JSON
     }
 });
 
+// DELETE post by id
 app.delete("/posts/:id", async (request, response) => {
-    const id = request.params.id;
-    const result = await Post.destroy({ where: { id: id } });
+    const id = request.params.id; // use id from url
+    const result = await Post.destroy({ where: { id: id } }); // DELETE FROM posts WHERE id = id;
 
     if (result) {
-        response.json({ message: "Post deleted" });
+        response.json({ message: "Post deleted" }); // send a success message as JSON
     } else {
         response.json({ message: "Post not found" });
     }
